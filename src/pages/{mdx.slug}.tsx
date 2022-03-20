2d5d68ad5b9ee2bf29a344ac00ae252985c31dae
Max <rjintao@gmail.com>
2022-03-20 20:54:32 +0300

    fix widow object

--- src/pages/{mdx.slug}.tsx
+++ src/pages/{mdx.slug}.tsx
@@ -7,9 +7,9 @@ import Layout from "../layout";
 import Bread from "../components/Bread";
 const MdxSlug = ({ data, location }: Props) => {
   const path = location.pathname
-  const win_path = window.location.pathname
+  // const win_path = window.location.pathname
   let isMap = path.slice(-3) === "map";
-  console.log(path, win_path);
+  // console.log(path, win_path);
 
 
   let { body, parent: { relativePath }, rawBody, h1, h2, frontmatter } = data.mdx;
